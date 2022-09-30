637767b105852d98fef2ff44c5dbe6553e6271fc
Theologos Mavropoulos <rusinner88@gmail.com>
2022-09-30 15:18:49 +0300

    final

--- main.js
+++ main.js
@@ -21,10 +21,19 @@ function calculateTip(){
 
  //calculate tip
  let totalTip = parseFloat((bill * (tipPercent / 100)).toFixed(2));
- let total = parseFloat((bill * totalTip).toFixed(2));
+ let total = parseFloat((bill + totalTip).toFixed(2));
 
  let tipPerPerson = (totalTip / noOfPeople).toFixed(2);
  let totalPerPerson = (total / noOfPeople).toFixed(2);
 
+ //display output values in screen
+ document.getElementById('tip-amount').textContent = `${totalTip} \€`;
+ document.getElementById('total-amount').textContent = `${total} \€`;
+ document.getElementById('tip-percent').textContent = `${tipPercent} %`;
+ document.getElementById('split-num').textContent = noOfPeople;
+ document.getElementById('tip-per-person').textContent =`${tipPerPerson} \€`;
+ document.getElementById('total-per-person').textContent = `${totalPerPerson}`; 
+
 }
 
+calculateTip();
